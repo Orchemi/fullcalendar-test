619bb3f33cbd72964355cacdae1ffb436b2afcdf
tmdgns1126 <tmdgns1126@naver.com>
2025-05-26 17:16:25 +0900

    Feat: fullcalendar 설치 및 초기 플러그인 매핑

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-export default function Home() {
+'use client';
+
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+export default function Calendar() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      Hello, World!
+    <div className="bg-white p-4">
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        height="auto"
+      />
     </div>
   );
 }
